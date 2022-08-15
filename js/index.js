5b0bc62f834a17c6150032380061a00a751465a0
GuilleTano <110430498+GuilleTano@users.noreply.github.com>
2022-08-15 14:53:37 -0300

    Se revirtieron todos los cambios anteriores

--- js/index.js
+++ js/index.js
@@ -1,31 +1,14 @@
-document.getElementById("loginBtn").addEventListener("click", function(){
-
-    let email = document.getElementById("email");
-    let pass = document.getElementById("password");
-
-    if (email.value == "" || pass.value == "") {
-        showAlertError();
-
-    }
-    else {
-        showAlertLogin();
-        loginRedirection();
-    }
-}
-);
-
-function showAlertLogin() {
-    
-    alert("Acceso correcto!");
-
-}
-
-function showAlertError() {
-
-    alert("Error en los datos ingresados");
-
-}
-
-function loginRedirection() {
-    window.location.href = "index.html";
-}
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("autos").addEventListener("click", function() {
+        localStorage.setItem("catID", 101);
+        window.location = "products.html"
+    });
+    document.getElementById("juguetes").addEventListener("click", function() {
+        localStorage.setItem("catID", 102);
+        window.location = "products.html"
+    });
+    document.getElementById("muebles").addEventListener("click", function() {
+        localStorage.setItem("catID", 103);
+        window.location = "products.html"
+    });
+});
---
+++ js/login.js
@@ -0,0 +1,31 @@
+document.getElementById("loginBtn").addEventListener("click", function(){
+
+    let email = document.getElementById("email");
+    let pass = document.getElementById("password");
+
+    if (email.value == "" || pass.value == "") {
+        showAlertError();
+
+    }
+    else {
+        showAlertLogin();
+        loginRedirection();
+    }
+}
+);
+
+function showAlertLogin() {
+    
+    alert("Acceso correcto!");
+
+}
+
+function showAlertError() {
+
+    alert("Error en los datos ingresados");
+
+}
+
+function loginRedirection() {
+    window.location.href = "index.html";
+}
