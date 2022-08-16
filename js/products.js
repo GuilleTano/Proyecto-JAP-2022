8aeae88c6471c9cd2f9def5e7e744239c18e97ab
GuilleTano <110430498+GuilleTano@users.noreply.github.com>
2022-08-16 14:35:35 -0300

    Se borraron comentarios innecesarios y formato

--- js/products.js
+++ js/products.js
@@ -1,11 +1,9 @@
-//let productosActuales = [];
-
 function showProducts() {
 
-    for (let producto of listaProductos.products){
-        let row="";
+    for (let producto of listaProductos.products) {
+        let row = "";
 
-        row= `
+        row = `
                 <div class="list-group-item list-group-item-action cursor-active">
                     <div class="row">
                         <div class="col-3">
@@ -22,47 +20,20 @@ function showProducts() {
 
                 </div>
             `;
-    
+
         document.getElementById("productsList").innerHTML += row;
-    }   
+    }
 
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    getJSONData(PRODUCT_101).then(function(resultado){
-        if (resultado.status === "ok"){
+document.addEventListener("DOMContentLoaded", function () {
+    getJSONData(PRODUCT_101).then(function (resultado) {
+        if (resultado.status === "ok") {
             listaProductos = resultado.data;
             showProducts();
         }
-        else{
+        else {
             //avisar del error con una alerta por ejemplo
         }
     });
 });
-
-
-/*
-    let htmlContentToAppend = "";
-    for (let i = 0; i < prodActuales.products.length; i++) {
-        let cosoProd = prodActuales.products[i];
-
-        htmlContentToAppend += `
-        <div onclick="setCatID(${cosoProd.id})" class="list-group-item list-group-item-action cursor-active">
-            <div class="row">
-                <div class="col-3">
-                    <img src="${cosoProd.image}" alt="${cosoProd.description}" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">${cosoProd.name}</h4>
-                        <small class="text-muted">${cosoProd.soldCount} artículos</small>
-                    </div>
-                    <p class="mb-1">${cosoProd.description}</p>
-                </div>
-            </div>
-        </div>    
-        `
-        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
-    }
-
-    */
