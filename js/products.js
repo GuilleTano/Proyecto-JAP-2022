491b6f6f77c03e9e03bda4848e3b0b8ff19db770
GuilleTano <110430498+GuilleTano@users.noreply.github.com>
2022-08-17 14:27:18 -0300

    Correcciones minimas de codigo y estilo

--- js/products.js
+++ js/products.js
@@ -11,11 +11,11 @@ function showProducts() {
                         </div>
                         <div class="col">
                             <div class="d-flex w-100 justify-content-between">
-                                <h4 class="mb-1">${producto.name}</h4>
-                                <small class="text-muted">${producto.soldCount} artículos</small>
-                            </div>          
+                                <h4 class="mb-1">${producto.name} - ${producto.currency} ${producto.cost}</h4>
+                                <small class="text-muted">${producto.soldCount} vendidos</small>
+                            </div>
+                            <p class="mb-1">${producto.description}</p>       
                         </div>
-                        <p class="mb-1">${producto.description}</p>
                     </div>
 
                 </div>
