1aa738b339e0d9ca922c48bb4d1c0be7ccf6c583
GuilleTano <guillermotano@hotmail.com>
2022-10-14 15:21:24 -0300

    alertas de login en proceso

--- js/login.js
+++ js/login.js
@@ -13,13 +13,28 @@ document.addEventListener("DOMContentLoaded", function(){
         if (email === "") {
             
             faltaDato = true;
-            alert("Debe ingresar un mail");
-    
+            let mailAlert= `
+            <div class="container">
+                <div class="alert alert-danger alert-dismissible text-center" role="alert">
+                    <div>Debe ingresar un mail valido</div>
+                    <button type="button" class="btn-close " data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+            `;
+            document.getElementById("show-alerts").innerHTML = mailAlert;
         }
         if (pass === "") {
                     
             faltaDato = true;
-            alert("Debe ingresar una clave");
+            let passAlert= `
+            <div class="container">
+                <div class="alert alert-danger alert-dismissible text-center" role="alert">
+                    <div>Debe ingresar un mail valido</div>
+                    <button type="button" class="btn-close " data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+            `;
+            document.getElementById("show-alerts").innerHTML = passAlert;
         }
         if(!faltaDato){
             alert("Acceso correcto!");
