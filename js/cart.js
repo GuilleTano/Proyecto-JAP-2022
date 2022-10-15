a5493b80d035cb5f2467c1899ed71eecc47c2c74
GuilleTano <110430498+GuilleTano@users.noreply.github.com>
2022-10-14 22:00:09 -0300

    Ultimos detalles del carrito

--- js/cart.js
+++ js/cart.js
@@ -1,38 +1,14 @@
 //------------------------------- CARRITO DE EJEMPLO -------------------------------
-let cantCarrito =0;
-let costoUnidad =0;
+function exampleCart(){
 
-/*
-function showActualCart(){
-    let defaultCart= "";
+    //let exampleProduct = actualCart.articles[0];
 
-    costoUnidad = actualCart.articles[0].unitCost;
-    cantCarrito = actualCart.articles[0].count;
-    
-    idCantidad = actualCart.articles[0].id;
-    idSubTotal = actualCart.articles[0].id + "ST";
+    let exampleProduct = new Carrito(actualCart.articles[0].id, actualCart.articles[0].image, actualCart.articles[0].name, actualCart.articles[0].currency, actualCart.articles[0].unitCost);
 
-    defaultCart =`<tr>
-    <td style="width:20%"><img src="${actualCart.articles[0].image}" width="80" ></td>
-    <td style="width:20%">${actualCart.articles[0].name}</td>
-    <td style="width:20%">${actualCart.articles[0].currency} ${costoUnidad}</td>
-    <td style="width:20%"><input type="text" class="form-control w-25" value="${cantCarrito}" onchange="cambiarCant()" id="${idCantidad}"></td>
-    <td style="width:20%"><strong>${actualCart.articles[0].currency} <span id="${idSubTotal}">${calcularSubTotal()}</span></strong></td>
-    </tr>
-    `;
-
-    document.getElementById("cartTable").innerHTML += defaultCart;
-}
-*/
-
-function calcularSubTotal(){
-
-    return costoUnidad * cantCarrito;;
-}
-function cambiarCant(){
-
-    let nuevoSubTot = document.getElementById(idCantidad).value * costoUnidad;
-    document.getElementById(idSubTotal).innerHTML = nuevoSubTot;
+    let cartList = [];
+    cartList.push(exampleProduct);
+    localStorage.setItem("cartList", JSON.stringify(cartList));
+    //return console.log(exampleProduct);
 }
 
 //------------------------------- CARRITO CON BOTON COMPRAR -------------------------------
@@ -42,15 +18,10 @@ function emptyCart(){
 }
 
 function showCart() {
-
+    
     let itemCartList = "";
-
     cartList = JSON.parse(localStorage.getItem("cartList"));
     console.log(cartList);
-
-    if(cartList.length < 1){
-        emptyCart();
-    }
 
     for (let i = 0; i < (cartList).length; i++) {
 
@@ -60,16 +31,16 @@ function showCart() {
             <td style="width:20%"><img src="${cartList[i].miniatura}" width="80" ></td>
             <td style="width:20%">${cartList[i].nombreP}</td>
             <td style="width:20%">${cartList[i].monedaP} ${cartList[i].costoP}</td>
-            <td style="width:20%"><input type="text" class="form-control w-25" value="${cartList[i].cantP}" oninput="cambiarCantidad(${cartList[i].idP}, this.value)"></td>
+            <td style="width:20%"><input type="text" class="form-control w-25" value="${cartList[i].cantP}" oninput="changeCount(${cartList[i].idP}, this.value)"></td>
             <td style="width:20%"><strong>${cartList[i].monedaP} ${cartList[i].subTotal()}</strong></td>
-            <td style="width:20%"><button type="button" class="btn btn-danger" id="boton_eliminar" onclick="borrarProducto(${cartList[i].idP})">Eliminar</button></td>
+            <td style="width:20%"><button type="button" class="btn btn-danger" id="boton_eliminar" onclick="deleteProduct(${cartList[i].idP})">Eliminar</button></td>
             </tr>
         `;
     }
     document.getElementById("cartTable").innerHTML = itemCartList;
 }
 
-function cambiarCantidad(productoID, nuevoValor){
+function changeCount(productoID, nuevoValor){
 
     cartList = JSON.parse(localStorage.getItem("cartList"));
 
@@ -84,37 +55,40 @@ function cambiarCantidad(productoID, nuevoValor){
     showCart();
 }
 
-function borrarProducto(productoID){
+function deleteProduct(productoID){
 
     cartList = JSON.parse(localStorage.getItem("cartList"));
 
     for (let i = 0; i < (cartList).length; i++){
         Object.setPrototypeOf(cartList[i], Carrito.prototype);
-
         if(cartList[i].idP === productoID){
-            
             cartList.splice((cartList).indexOf(cartList[i]), 1);
         }
     }
-    localStorage.setItem("cartList", JSON.stringify(cartList));
-    showCart();
+    if(cartList.length < 1){
+        localStorage.removeItem("cartList");
+        emptyCart();
+    }
+    else{
+        localStorage.setItem("cartList", JSON.stringify(cartList));
+        showCart();
+    }
 }
 
-
 document.addEventListener("DOMContentLoaded", function(){
-
+    
     if(localStorage.getItem("cartList")){
         showCart();
     }
     else{
         emptyCart();
     }
-
+    
     //LLAMADA AL CARRITO DEL SERVIDOR
     getJSONData(CART_PRUEBAS).then(function (resultado) {
         if (resultado.status === "ok") {
             actualCart = resultado.data;
-            //showActualCart();
+            //exampleCart();
             //console.log(actualCart);
 
         }
